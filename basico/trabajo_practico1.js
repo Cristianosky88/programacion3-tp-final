fb0225504dc4e6364d7685c1e433cdb419a064e2
Cristianosky88 <cristian_skater_6@hotmail.com>
2025-08-04 17:51:39 -0300

    Se realiza primer tp

---
+++ basico/trabajo_practico1.js
@@ -0,0 +1,8 @@
+// Precio_del_producto
+let precio = 300;
+let iva = 21;
+
+// Calcular el precio total incluyendo IVA
+let precioTotal = precio + (precio * iva / 100);
+
+console.log("Precio total: $" + precioTotal);
