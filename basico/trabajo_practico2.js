861cab9851d0153f3da974d4492532c29a9015c1
Cristianosky88 <cristian_skater_6@hotmail.com>
2025-08-04 17:59:13 -0300

    Se realiza el agregado de los restante tp basicos

---
+++ basico/trabajo_practico2.js
@@ -0,0 +1,11 @@
+// Ejercicio 2: Área y perímetro de un cuadrado
+let lado = 5;
+
+let area = lado * lado;
+let perimetro = lado * 4;
+
+console.log("Lado del cuadrado: " + lado);
+console.log("Área: " + area);
+console.log("Perímetro: " + perimetro);
+
+console.log("-------------------");
